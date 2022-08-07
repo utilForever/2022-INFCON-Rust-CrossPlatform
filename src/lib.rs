366ac4b4c836dcd63446eb43e35bdea42880ece0
Chris Ohk <utilforever@gmail.com>
2022-08-07 23:47:19 +0900

    feat: Add functions 'add()' and 'sub()'

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn add(a: i64, b: i64) -> i64 {
+    a + b
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn sub(a: i64, b: i64) -> i64 {
+    a - b
 }
