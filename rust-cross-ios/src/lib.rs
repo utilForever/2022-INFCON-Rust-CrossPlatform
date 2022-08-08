e7fce1a2b504986232f02c214cb4dfeccc1a7949
Chris Ohk <utilforever@gmail.com>
2022-08-08 17:40:54 +0900

    feat: Add code to support C bridge functions

--- rust-cross-ios/src/lib.rs
+++ rust-cross-ios/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[no_mangle]
+pub extern "C" fn add(a: i64, b: i64) -> i64 {
+    rust_cross::add(a, b)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn sub(a: i64, b: i64) -> i64 {
+    rust_cross::sub(a, b)
 }
