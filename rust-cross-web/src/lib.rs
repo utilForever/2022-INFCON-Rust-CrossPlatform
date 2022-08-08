27cd2b4e453bcb107e68902fe907cc8f1e4c7ed5
Chris Ohk <utilforever@gmail.com>
2022-08-08 23:00:22 +0900

    feat: Add code to support WebAssembly

--- rust-cross-web/src/lib.rs
+++ rust-cross-web/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i64, b: i64) -> i64 {
+    rust_cross::add(a, b)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn sub(a: i64, b: i64) -> i64 {
+    rust_cross::sub(a, b)
 }
